5bd615761d718751dfeaa12c5b247f44ce2d2bbd
Pazmy <fadhillah17azmy01@gmail.com>
2021-11-22 15:32:59 +0700

    init backend

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT | 3001;
+
+app.listen(PORT, () => console.log(`listen on ${PORT}`));
