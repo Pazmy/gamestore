4046c4a85f4e0deea0ac76be6cd891c9d2cbb0ea
Pazmy <49399816+Pazmy@users.noreply.github.com>
2021-11-23 16:16:59 +0700

    backend:authentication and user routes (#5)

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const router = require("./routes");
+app.use(router);
+
 const PORT = process.env.PORT | 3001;
 
 app.listen(PORT, () => console.log(`listen on ${PORT}`));
